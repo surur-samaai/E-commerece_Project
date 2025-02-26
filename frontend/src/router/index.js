72563b96f97f68e43bc19cafa8042298ca56c656
TwinnJoe <sinovuyojoe@gmail.com>
2025-02-26 15:06:33 +0200

    🔃

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,13 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Articles from '../views/Articles.vue';
 import Booking from '../views/Booking.vue';
+import Cart from '../views/Cart.vue';
 import Shop from '../views/Shop.vue';
+import Payment from '@/views/Payment.vue';
+import Plans from '@/views/Plans.vue';
+import Products from '@/views/Products.vue';
+import Trainers from '@/views/Trainers.vue';
+import Tutorials from '@/views/Tutorials.vue';
 
 const routes = [
   { path: '/',
@@ -25,6 +31,30 @@ const routes = [
   { path: '/shop', 
     name:Shop,
     component: Shop 
+  },
+  { path: '/cart', 
+    name:Cart,
+    component: Cart 
+  },
+  { path: '/payment', 
+    name:Payment,
+    component: Payment 
+  },
+  { path: '/plans', 
+    name:Plans,
+    component: Plans 
+  },
+  { path: '/products', 
+    name:Products,
+    component: Products 
+  },
+  { path: '/trainers', 
+    name:Trainers,
+    component: Trainers 
+  },
+  { path: '/tutorials', 
+    name:Tutorials,
+    component: Tutorials 
   },
 ];
 
